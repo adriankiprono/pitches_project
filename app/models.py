747b17a3d053924953ceeabdf4418cb209385743
adriankiprono <tuimuradrian6@gmail.com>
2019-11-25 14:39:17 +0300

    add colums tothe class users

--- app/models.py
+++ app/models.py
@@ -4,6 +4,13 @@ class User(db.Model):
     __tablename__ ="users"
     id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(255), nullable=False, unique=True)
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+    
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    date_joined = db.Column(db.DateTime,default=datetime.utcnow)
+    
+    pitches =  db.relationship('Pitch', backref = 'user', lazy = "dynamic")
 
     def __repr__(self):
         return f'User{self.username}'
@@ -31,6 +38,18 @@ class Pitch(db.Model):
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
+    @classmethod
+    def get_pitches(cls,catergory):
+        pitches  = Pitch.query.filter_by(id=id).first()
+        return pitches
+
+    @classmethod
+    def get_pitches(cls,id):
+        pitches = Pitch.query.filter_by(user_id = id).all()
+        return pitches
+         
+    
+
 
 
 
