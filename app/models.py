88bc1ae0c4427b0e7026b96f91aa103b79bfcfc6
adriankiprono <tuimuradrian6@gmail.com>
2019-11-25 14:13:00 +0300

    add the class pitches

--- app/models.py
+++ app/models.py
@@ -7,6 +7,31 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User{self.username}'
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+class Pitch(db.Model):
+    __tablename__ ='pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_title = db.Column(db.String)
+    pitch_content = db.Column(db.String(800))
+    category = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
 
 
 
