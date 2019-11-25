0d5ce895fe34ff4dc682057785f8ae428dfbf97d
adriankiprono <tuimuradrian6@gmail.com>
2019-11-25 14:42:41 +0300

    add the changes to the views

--- app/main/views.py
+++ app/main/views.py
@@ -10,12 +10,17 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    # Getting popular movie
+    # Getting the categories of pitches
     
     title = 'Home - Welcome to The best  Place to Pitch Website Online'
 
+    interview_pitches = Pitch.get_pitches('interview')
+    product_pitches = Pitch.get_pitches('product')
+    promotion_pitches = Pitch.get_pitches('promotion')
+
+
     
-    return render_template('index.html' )
+    return render_template('index.html' ,title = title ,interview= interview_pitches,product = product_pitches,promotion = promotion_pitches  )
 
 
 
