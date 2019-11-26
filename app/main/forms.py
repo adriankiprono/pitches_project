38e254ad7ea34ee06d4cb67c7b0a01046c8f3395
adriankiprono <tuimuradrian6@gmail.com>
2019-11-26 09:21:25 +0300

    add the pitchform with category

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+class PitchForm(FlaskForm):
 
     title = StringField('Review title',validators=[Required()])
     review = TextAreaField('Movie review', validators=[Required()])
+    category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('promotion','Promotion pitch')],validators=[Required()])
     submit = SubmitField('Submit')
