9b89a06623381b7a89b60a0b07c0d4fed8452316
adriankiprono <tuimuradrian6@gmail.com>
2019-11-26 09:08:25 +0300

    add dynamic route to pitch ,interview ,product and promotion

--- app/main/views.py
+++ app/main/views.py
@@ -23,6 +23,24 @@ def index():
     
     return render_template('index.html' ,title = title ,interview= interview_pitches,product = product_pitches,promotion = promotion_pitches  )
 
+@main.route('/pitch/new', methods = ['GET','POST'])
+def new_pitch():
+    pitch_form = PitchForm()
+    if pitch_form.validate_on_submit():
+        title = pitch_form.title.data
+        pitch = pitch_form.text.data
+        category = pitch_form.category.data
+
+        # Updated pitch instance
+        new_pitch = Pitch(pitch_title=title,pitch_content=pitch,category=category,user=current_user,likes=0,dislikes=0)
+
+        # Save pitch method
+        new_pitch.save_pitch()
+        return redirect(url_for('.index'))
+
+    title = 'New pitch'
+    return render_template('new_pitch.html',title = title,pitch_form=pitch_form )
+
 
 
 
