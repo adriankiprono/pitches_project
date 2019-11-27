9653a6f2e3d6eb529cff6bbf95bc456301555964
adriankiprono <tuimuradrian6@gmail.com>
2019-11-27 10:43:39 +0300

    add configuration to the config

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adriantuimur://qweasdzxc1234567890@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     @staticmethod
     def init_app(app):
